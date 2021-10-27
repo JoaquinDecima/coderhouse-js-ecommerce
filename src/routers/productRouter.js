35892df226302188f281351dabdc11a4f43e6acf
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-26 22:52:51 -0300

    Add productManager to productRouter

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import ProductManager from '../managers/productManager.js';
 
 const productRouter = express.Router();
+const productManager = new ProductManager();
 
 export default productRouter;
