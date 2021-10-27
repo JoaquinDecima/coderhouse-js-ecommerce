0122958908d0b5646ed1b1fd007e320fb07cac6f
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-26 22:24:00 -0300

    Add Routers server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import cartRouter from './routers/cartRouter.js';
+import productRouter from './routers/productRouter.js';
 
 // SetUp del entorno
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Configuro la App Express
 app.use(express.urlencoded({ extended: true }));
 
+// Configuro las Rutas
+app.use('/api/carrito', cartRouter);
+app.use('/api/productos', productRouter);
+
+// Inicio la Aplicacion
 app.listen(PORT, () => {
-  console.log(`Aplicacion inicio en https://localhost:${PORT}`)
+  console.log(`Aplicacion inicio en http://localhost:${PORT}`);
 })
