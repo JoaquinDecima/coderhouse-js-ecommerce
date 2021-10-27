4b3402f56a3009b3554204d39994d1dff19f9653
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-27 09:26:37 -0300

    Add admin exclusive routers

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productManager from '../managers/productManager.js';
+import { isAdmin } from '../utils/userRank.js'
 
 const productRouter = express.Router();
 
@@ -15,35 +16,48 @@ productRouter.get('/:id', function(req, res, next){
 
 // Retorno la id del producto creado
 productRouter.post('/', function(req, res, next){
-  const id = productManager.addProdcut(
-    req.body.nombre,
-    req.body.descripcion,
-    req.body.codigo,
-    req.body.foto,
-    req.body.precio,
-    req.body.stock);
-  res.send({id});
+  if (isAdmin('')) {
+    const id = productManager.addProdcut(
+      req.body.nombre,
+      req.body.descripcion,
+      req.body.codigo,
+      req.body.foto,
+      req.body.precio,
+      req.body.stock);
+    res.send({id});
+  }else {
+    res.send({error:'ruta /api/products/:id con metodo POST no autorizada'});
+  }
 });
 
 // Elimina el porducto con id :id
 productRouter.delete('/:id', function(req, res, next){
-  res.send(productManager.removeProductById(req.params.id));
+  if (true) {
+    res.send(productManager.removeProductById(req.params.id));
+  }else {
+    res.send({error:'ruta /api/products/:id con metodo DELETE no autorizada'});
+  }
 });
 
 // Edita el producto con id :id
 productRouter.put('/:id', function(req, res, next){
-  if(productManager.editPorductByID(
-    req.params.id,
-    req.body.nombre,
-    req.body.descripcion,
-    req.body.codigo,
-    req.body.foto,
-    req.body.precio,
-    req.body.stock)){
-    res.send("");
-  }else{
-    res.send({error:'Producto no encontrado'});
+  if (true) {
+    if(productManager.editPorductByID(
+      req.params.id,
+      req.body.nombre,
+      req.body.descripcion,
+      req.body.codigo,
+      req.body.foto,
+      req.body.precio,
+      req.body.stock)){
+      res.send("");
+    }else{
+      res.send({error:'Producto no encontrado'});
+    }
+  } else {
+    res.send({error:'ruta /api/products/:id con metodo PUT no autorizada'});
   }
+
 });
 
 // Exporto la ruta necesaria
