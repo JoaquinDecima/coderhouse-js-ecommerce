90db70e5779000c7ed46acd5dc21c85e5cf7f64b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-27 09:20:03 -0300

    Add Util userRank

---
+++ src/utils/userRank.js
@@ -0,0 +1,7 @@
+// Utilidad a modo ejemplo que determina si un usuario es admin o no
+// ATTENCION: Actualmente esto returna true siempre
+function isAdmin(user){
+  return (true || false);
+}
+
+export {isAdmin};
