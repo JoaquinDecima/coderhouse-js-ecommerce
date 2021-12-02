da322935e2013ee415e4f4f6103b779ac11ad90a
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-12-02 11:19:13 -0300

    fix mongoManager.js Inizialization Add DeleteByID

--- src/managers/mongoManager.js
+++ src/managers/mongoManager.js
@@ -2,7 +2,7 @@ import Mongodb from "mongodb";
 
 export default class MongoManager{
     constructor(uri, db, collection){
-        this.client = Mongodb.MongoClient(uri);
+        this.client = new Mongodb.MongoClient(uri);
         this.db = db;
         this.collection = collection;
     }
@@ -27,11 +27,31 @@ export default class MongoManager{
         }
     }
 
+    async deleteByID(id){
+        try {
+            await this.client.connect();
+            await this.client.db(this.db).collection(this.collection).deleteOne({_id:id})
+        }finally {
+            await this.client.close()
+        }
+    }
+
     async readData(){
         let returner;
         try {
             await this.client.connect();
-            returner = await this.client.db(this.db).collection(this.collection).find();
+            returner = await this.client.db(this.db).collection(this.collection).find().toArray();
+        } finally {
+            await this.client.close();
+            return returner;
+        }
+    }
+
+    async readDataByID(id){
+        let returner;
+        try {
+            await this.client.connect();
+            returner = await this.client.db(this.db).collection(this.collection).find({_id:id}).toArray();
         } finally {
             await this.client.close();
             return returner;
