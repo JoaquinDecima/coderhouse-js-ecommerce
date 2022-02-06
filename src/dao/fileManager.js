e9beca8931970315e222c4d261e4d856bde5e5c1
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-02-06 12:05:36 -0300

    Fix eslint

---
+++ src/dao/fileManager.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+export default class FileManager{
+	constructor(path){
+		this.path = path;
+		this.check();
+	}
+
+	async check(path, callback) {
+		try {
+			await fs.access(this.path, 'utf8');
+		} catch {
+			fs.writeFileSync(this.path,'[]');
+		}
+	}
+
+	writeData(data){
+		fs.writeFileSync(this.path, data, 'utf8');
+	}
+
+	readData(){
+		return fs.readFileSync(this.path);
+	}
+}
