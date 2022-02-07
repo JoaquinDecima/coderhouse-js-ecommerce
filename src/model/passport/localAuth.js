adbaeaa5ccd2cd40f482175219e53e0e917cfebe
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-02-07 13:50:36 -0300

    Add Logs to Register

--- src/model/passport/localAuth.js
+++ src/model/passport/localAuth.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import { usersData } from '../../instances.js';
+import { logger } from '../tools/logger.js';
 
 // Registro local con Passport
 passport.use('register', new LocalStrategy({
@@ -20,13 +21,16 @@ passport.use('register', new LocalStrategy({
 	};
 
 	if ([] == usersData.getUserByID(email)){
+		logger.warn(`Usuario ${email} ya existe`);
 		return done(true,'Usuario Existente');
 	}else{
 		usersData.addUser(user)
 			.then(()=>{
+				console.info(`Se registro exitosamente ${email}`);
 				return done(null, user);
 			})
 			.catch(err => {
+				logger.error(`No se pudo registrar ${email} debido a ${err}`);
 				return done(true, err);
 			});
 	}
@@ -42,7 +46,7 @@ passport.serializeUser((user, done)=>{
 passport.deserializeUser((id, done)=>{
 	usersData.getUserByID(id)
 		.then(user =>{
-			return done(null, user);
+			return done(null, user[0]);
 		})
 		.catch(err => {
 			return done(true, err);
