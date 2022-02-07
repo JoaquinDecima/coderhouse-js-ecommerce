5c2807b3e962d15ef5f1161e5bb63eb7f3fd7012
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-02-07 13:39:09 -0300

    Add 404 View

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -2,12 +2,16 @@ import express from 'express';
 import { logger } from '../model/tools/logger.js';
 import cartRouterAPI from './cartRouterAPI.js';
 import productRouterAPI from './productRouterAPI.js';
+import authRouterAPI from './authRouterAPI.js';
+import errorRouter from './errorRouter.js';
 
 const globalRouter = express.Router();
 
 // Use other Routers
 globalRouter.use('/api/carrito', cartRouterAPI);
 globalRouter.use('/api/productos', productRouterAPI);
+globalRouter.use('/api/auth/', authRouterAPI);
+globalRouter.use('/error/', errorRouter);
 
 globalRouter.get('/', (req,res)=>{
 	logger.info(`[GET] se ingreso en ${req.url}`);
@@ -17,7 +21,7 @@ globalRouter.get('/', (req,res)=>{
 // Se definen casos por defecto
 globalRouter.get('*', (req, res) => {
 	logger.warn(`[GET] Ruta no encontrada ${req.url}`);
-	res.status(404).send({error:-2, descripcion:'Ruta no implementada'});
+	res.status(404).render('errors/404');
 });
 
 export default globalRouter;
