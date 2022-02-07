b4312910b16c6871c98c94e6a7d50db2047421d5
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-02-07 12:19:50 -0300

    Create logger.js

---
+++ src/model/tools/logger.js
@@ -0,0 +1,30 @@
+import winston from 'winston';
+
+class Logger{
+	constructor(){
+		this.logger = winston.createLogger({
+			format: winston.format.simple(),
+			transports: [
+				new winston.transports.File({ filename: '/log/error.log', level: 'error' }),
+				new winston.transports.File({ filename: '/log/warn.log', level: 'warn' }),
+				new winston.transports.Console({
+					format: winston.format.combine(winston.format.colorize(), winston.format.simple())
+				})
+			]
+		});
+	}
+
+	warn(mensaje){
+		this.logger.warn(mensaje);
+	}
+
+	error(mensaje){
+		this.logger.error(mensaje);
+	}
+
+	info(mensaje){
+		this.logger.info(mensaje);
+	}
+}
+
+export const logger = new Logger();
