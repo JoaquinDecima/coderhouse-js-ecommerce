191bbc43a4902bbd70c32d0908dcd23e977db29b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-02-21 09:21:01 -0300

    Add error permisos

--- src/routers/errorRouter.js
+++ src/routers/errorRouter.js
@@ -4,14 +4,19 @@ import {logger} from '../model/tools/logger.js';
 
 const errorRouter = express.Router();
 
-errorRouter.get('/register',isNotAuthenticated, (req, res)=>{
+errorRouter.get('/register', isNotAuthenticated, (req, res)=>{
 	logger.info(`[GET] se ingreso en /error${req.url}`);
 	res.render('errors/register');
 });
 
-errorRouter.get('/login',isNotAuthenticated, (req, res)=>{
+errorRouter.get('/login', isNotAuthenticated, (req, res)=>{
 	logger.info(`[GET] se ingreso en /error${req.url}`);
 	res.render('errors/login');
+});
+
+errorRouter.get('/permisos', (req, res)=>{
+	logger.info(`[GET] se ingreso en /error${req.url}`);
+	res.render('errors/permisos');
 });
 
 export default errorRouter;
