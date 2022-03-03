40018b89e7f29f8e79116f0631459c1dd2622cc3
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-03 19:55:28 -0300

    Fix instances.js cartData

--- src/instances.js
+++ src/instances.js
@@ -4,5 +4,5 @@ import UserMongoManager from './model/user/userMongoManager.js';
 import 'dotenv/config';
 
 export const usersData = new UserMongoManager(process.env.MONGO_USERS);
-export const cartsData = new CartMongoManager(process.env.MONGO_CARTS);
 export const productsData = new ProductMongoManager(process.env.MONGO_PRODUCTS);
+export const cartsData = new CartMongoManager(process.env.MONGO_CARTS, productsData);
