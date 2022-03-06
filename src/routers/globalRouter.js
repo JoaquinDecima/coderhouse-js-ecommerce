b2fb3253129190fa015ea6a4d6a878e0c6c621ce
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-06 17:06:18 -0300

    Move API to globalRouterAPI.js

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,19 +1,15 @@
 import express from 'express';
 import { logger } from '../model/tools/logger.js';
 import authRouter from './authRouter.js';
-import authRouterAPI from './authRouterAPI.js';
-import cartRouterAPI from './cartRouterAPI.js';
-import productRouterAPI from './productRouterAPI.js';
 import errorRouter from './errorRouter.js';
 import productRouter from './productRouter.js';
+import globalRouterAPI from './globalRouterAPI.js';
 
 
 const globalRouter = express.Router();
 
 // Use other Routers
-globalRouter.use('/api/auth/', authRouterAPI);
-globalRouter.use('/api/cart', cartRouterAPI);
-globalRouter.use('/api/products', productRouterAPI);
+globalRouter.use('/api/', globalRouterAPI);
 globalRouter.use('/products/', productRouter);
 globalRouter.use('/auth/', authRouter);
 globalRouter.use('/error/', errorRouter);
---
+++ src/routers/globalRouterAPI.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import authRouterAPI from './authRouterAPI.js';
+import cartRouterAPI from './cartRouterAPI.js';
+import productRouterAPI from './productRouterAPI.js';
+
+const globalRouterAPI = express.Router();
+
+globalRouterAPI.use('/auth/', authRouterAPI);
+globalRouterAPI.use('/cart/', cartRouterAPI);
+globalRouterAPI.use('/products', productRouterAPI);
+
+export default globalRouterAPI;
