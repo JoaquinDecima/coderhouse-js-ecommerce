05b6dfe679ee93609ce6b782e78c6214414fac16
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-09 10:12:27 -0300

    Fix log cartRouterAPI.js

--- src/routers/cartRouterAPI.js
+++ src/routers/cartRouterAPI.js
@@ -6,13 +6,14 @@ const cartRouterAPI = express.Router();
 
 // Crea un carrito nuevo y retorna su ID
 cartRouterAPI.post('/', function(req, res){
+	logger.info(`[POST] se ingreso en /api/cart${req.url}`);
 	cartsData.addCart(req.body.id);
 	res.send({id : req.body.id});
 });
 
 // Elimina el carrito con id :id
 cartRouterAPI.delete('/:id', function(req, res){
-	logger.info(`[DELETE] se ingreso en ${req.url}`);
+	logger.info(`[DELETE] se ingreso en /api/cart${req.url}`);
 	cartsData.removeCartById(req.params.id)
 		.then(result =>{
 			logger.info(`Se Borro correctamente carrito ${req.params.id} - ${result}`);
@@ -26,7 +27,7 @@ cartRouterAPI.delete('/:id', function(req, res){
 
 // Retorna los productos del carrito con id :id
 cartRouterAPI.get('/:id/productos', function(req, res){
-	logger.info(`[GET] se ingreso en ${req.url}`);
+	logger.info(`[GET] se ingreso en /api/cart${req.url}`);
 	cartsData.getProductsOfCartWhitID(req.params.id)
 		.then(result =>{
 			logger.info(`Se obtubieron correctamente los productos del carrito ${req.params.id} - ${result}`);
@@ -40,7 +41,7 @@ cartRouterAPI.get('/:id/productos', function(req, res){
 
 // Agrega el producto con id productID al carrito con id :id
 cartRouterAPI.post('/:id/productos', function(req, res){
-	logger.info(`[POST] se ingreso en ${req.url}`);
+	logger.info(`[POST] se ingreso en /api/cart${req.url}`);
 	cartsData.addProductsOfCartWhitID(req.params.id, req.body.productID)
 		.then(result => {
 			logger.info(`Se agrego correctamente producto ${req.body.productID} al carrito ${req.params.id} - ${result}`);
@@ -54,7 +55,7 @@ cartRouterAPI.post('/:id/productos', function(req, res){
 
 // Remueve los productos con id id_prod del carrito con id :id
 cartRouterAPI.delete('/:id/productos/:id_prod', function(req, res){
-	logger.info(`[DELETE] se ingreso en ${req.url}`);
+	logger.info(`[DELETE] se ingreso en /api/cart${req.url}`);
 	cartsData.removeProductsOfCartWhitID(req.params.id, req.params.productID)
 		.then(result => {
 			logger.info(`Se borró correctamente producto ${req.body.productID} al carrito ${req.params.id} - ${result}`);
