bcf21d5602f2f319b8c70d8fcfcac7c6cc2632da
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-09 16:17:34 -0300

    Add Buy email

--- src/model/tools/writeMail.js
+++ src/model/tools/writeMail.js
@@ -20,3 +20,61 @@ export function register(user){
     </html>
     `;
 }
+
+export function compra(products, nombre, correo, telefono, direccion){
+	let total = 0;
+	let html = `
+    <html>
+        <head>
+            <title>Se realizo una nueva compra </title>
+        </head>
+        <body>
+            <h1>Nuevo pedido de ${nombre} ${correo}</h1>
+            <p>
+                Se registro un nuevo pedido
+            </p>
+            <ul>
+                <li><b>Email:</b> ${correo}</li>
+                <li><b>Nombre:</b> ${nombre} </li>
+                <li><b>Telefono:</b> ${telefono}</li>
+                <li><b>Direccion:</b> ${direccion}</li>
+            </ul>
+        	<p>El pedido esta compuesto por:</p>
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Nombre</th>
+						<th>Precio</th>
+						<th>Cantidad</th>
+						<th>Total</th>
+					</tr>
+				</thead>
+				<tbody>
+    `;
+
+	products.forEach(product =>{
+		html = html.concat(`
+					<tr>
+						<th>${product._id}</th>
+						<td>${product.nombre}</td>
+						<td>$ ${product.precio}</td>
+						<td>${product.cant}</td>
+						<td>$ ${parseInt(product.cant, 10) * parseInt(product.precio, 10)}</td>
+					</tr>
+		`);
+		total +=  (parseInt(product.cant, 10) * parseInt(product.precio, 10));
+	});
+
+	html = html.concat(`
+					<tr>
+						<th colspan="4">Total</th>
+						<td>$ ${total}</td>
+					</tr>
+				</tbody>
+			</table>
+		</body>
+    </html>`);
+
+	return html;
+}
