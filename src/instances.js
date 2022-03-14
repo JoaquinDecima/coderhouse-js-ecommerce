caec9cc1fdfad568231ad89508c4c4c37f96a6ac
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-14 13:43:14 -0300

    Add instancia Chat

--- src/instances.js
+++ src/instances.js
@@ -1,4 +1,5 @@
 import CartMongoManager from './model/cart/cartMongoManager.js';
+import ChatMongoManager from './model/chat/chatMongoManager.js';
 import ProductMongoManager from './model/product/productMongoManager.js';
 import UserMongoManager from './model/user/userMongoManager.js';
 import 'dotenv/config';
@@ -6,3 +7,4 @@ import 'dotenv/config';
 export const usersData = new UserMongoManager(process.env.MONGO_USERS);
 export const productsData = new ProductMongoManager(process.env.MONGO_PRODUCTS);
 export const cartsData = new CartMongoManager(process.env.MONGO_CARTS, productsData);
+export const chatData = new ChatMongoManager(process.env.MONGO_CHAT);
