d524fb0d72a3a6ba823a2cda38d122339fe21a24
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2023-03-31 15:52:26 -0300

    Update Email

--- src/tools/sendMail.js
+++ src/tools/sendMail.js
@@ -4,16 +4,16 @@ const transporter = nodemailer.createTransport({
 	host: 'smtp.ethereal.email',
 	port: 587,
 	auth: {
-		user: 'osbaldo.ferry4@ethereal.email',
-		pass: 'AJkAPsbWZrstepPpvj'
-	}
+		user: 'mireille.kuphal@ethereal.email',
+		pass: 'zvhkS5J9AzwpdYUjwC',
+	},
 });
 
-export default async function sendMail(mail, destination, sender, title){
+export default async function sendMail(mail, destination, sender, title) {
 	transporter.sendMail({
 		from: sender,
 		to: destination,
 		html: mail,
-		subject: title
+		subject: title,
 	});
 }
